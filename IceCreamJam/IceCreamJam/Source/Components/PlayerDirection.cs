d42c27f5922fb64a89e4dbb9820a40fe0eed7edd
lightningstudios126 <ethan.wong126@gmail.com>
2020-03-21 00:30:27 -0400

    set up new animation system

---
+++ IceCreamJam/IceCreamJam/Source/Components/PlayerDirection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nez;
+
+namespace IceCreamJam.Source.Components {
+	class PlayerDirection : Component {
+		private Direction8 direction = Direction8.West;
+
+		internal Direction8 Direction {
+			get => this.direction; set {
+				if (this.direction != value) {
+					OnDirectionChange?.Invoke(value);
+				}
+				this.direction = value;
+			}
+		}
+
+		public event Action<Direction8> OnDirectionChange;
+		public enum Direction8 {
+			East, SouthEast, South, SouthWest, West, NorthWest, North, NorthEast, 
+		}
+	}
+}
--- IceCreamJam/IceCreamJam/Source/Entities/Truck.cs
+++ IceCreamJam/IceCreamJam/Source/Entities/Truck.cs
@@ -1,60 +1,30 @@
 ﻿using IceCreamJam.Source.Components;
-using IceCreamJam.Source.Content;
 using IceCreamJam.Source.WeaponSystem;
 using IceCreamJam.Source.WeaponSystem.Weapons;
-using Microsoft.Xna.Framework.Graphics;
 using Nez;
 using Nez.Sprites;
-using Nez.Textures;
-using System;
-using System.Collections.Generic;
 
 namespace IceCreamJam.Source.Entities {
-    class Truck : Entity {
+	class Truck : Entity {
 
-        public WeaponComponent weapons;
+		public override void OnAddedToScene() {
+			base.OnAddedToScene();
 
-        private ArcadeRigidbody rb;
-        private PlayerMovementComponent moveComponent;
+			this.Name = "Truck";
 
-        private SpriteAnimator animator;
-        private List<Sprite> sprites;
+			AddComponent(new PlayerDirection());
 
-        public override void OnAddedToScene() {
-            base.OnAddedToScene();
+			AddComponent(new SpriteAnimator());
+			AddComponent(new PlayerAnimationComponent());
 
-            this.Name = "Truck";
+			var collider = AddComponent(new BoxCollider());
+			collider.PhysicsLayer = (int)Constants.PhysicsLayers.Player;
+			collider.CollidesWithLayers = (int)Constants.PhysicsLayers.Buildings;
 
-            LoadSprites();
-            this.animator = AddComponent(new SpriteAnimator(sprites[0]));
+			AddComponent(new ArcadeRigidbody() { ShouldUseGravity = false, Elasticity = 0 });
+			AddComponent(new PlayerMovementComponent());
 
-            var collider = AddComponent(new BoxCollider());
-            collider.PhysicsLayer = (int)Constants.PhysicsLayers.Player;
-            collider.CollidesWithLayers = (int)Constants.PhysicsLayers.Buildings;
-
-            this.rb = AddComponent(new ArcadeRigidbody() { ShouldUseGravity = false, Elasticity = 0 });
-			this.moveComponent = AddComponent(new PlayerMovementComponent());
-
-            weapons = AddComponent(new WeaponComponent(new TestWeapon(), new TestWeapon2()));
-        }
-
-        private void LoadSprites() {
-            sprites = new List<Sprite>();
-
-            for(int i = 1; i < 9; i++) {
-                sprites.Add(new Sprite(Scene.Content.LoadTexture(ContentPaths.Truck + i + "a.png")));
-                Debug.Log(ContentPaths.Truck + i + "a.png");
-            }
-        }
-
-        public override void Update() {
-            base.Update();
-
-            var deg = -moveComponent.rotationDegrees % 360;
-            int spriteIndex = (int)(deg / 45);
-            spriteIndex = Utility.Mod(spriteIndex, 8);
-            animator.SetSprite(sprites[spriteIndex]);
-
-        }
-    }
+			AddComponent(new WeaponComponent(new TestWeapon(), new TestWeapon2()));
+		}
+	}
 }
